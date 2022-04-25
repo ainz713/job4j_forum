ce8b00a16da5206f13748dc2beeb162cfd0062bc
ainz713 <ainz713@gmail.com>
2022-04-25 18:47:10 +0300

    0. Spring Liquibase

--- src/main/resources/db/security_schema.sql
+++ src/main/resources/db/security_schema.sql
@@ -1,15 +1,3 @@
-CREATE TABLE authorities(
-                            id SERIAL PRIMARY KEY,
-                            name VARCHAR(50) NOT NULL UNIQUE
-);
-
-CREATE TABLE users(
-                      id SERIAL PRIMARY KEY,
-                      username VARCHAR(50) NOT NULL UNIQUE,
-                      password VARCHAR(100) NOT NULL,
-                      enabled BOOLEAN DEFAULT true,
-                      authority_id INT NOT NULL REFERENCES authorities(id)
-);
 
 INSERT INTO authorities (name) VALUES ('ROLE_USER');
 INSERT INTO authorities (name) VALUES ('ROLE_ADMIN');
